ed26dd19404170c74ba210fc6dbba2fcc4dfd6db
myfishdrowned <saif.rahman.va@gmail.com>
2025-01-04 11:44:37 -0500

    continued working on library project

---
+++ library/walkthrough/books.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+
+function Book() {
+    // this.title = title;
+    // this.author = author;
+    // this.pages = pages;
+    // this.read = read;
+
+    function addBookToLibrary() {
+        // let title = document.querySelector('#title').value;
+        // let author = document.querySelector('#author').value;
+        // let pages = document.querySelector('#pages').value;
+        // let read = document.querySelector('#read').value;
+        // let newBook = new Book(title, author, pages, read);
+        // console.log(newBook);
+
+    }
+
+    let newBookBtn = document.querySelector('#new-book-btn')
+    newBookBtn.addEventListener('click', function () {
+        let newBookForm = document.querySelector('#new-book-form');
+        newBookForm.style.display = 'flex'
+
+    })
